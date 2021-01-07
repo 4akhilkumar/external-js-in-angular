c03183aa64a01810ef15e4dd342c54f80319e07e
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-08 00:36:49 +0530

    declare function in the .js file in .ts file and declare the method in a method

--- src/app/js-validation-form/js-validation-form.component.ts
+++ src/app/js-validation-form/js-validation-form.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const validateform: any;
+
 @Component({
   selector: 'app-js-validation-form',
   templateUrl: './js-validation-form.component.html',
   styleUrls: ['./js-validation-form.component.css']
 })
 export class JsValidationFormComponent implements OnInit {
+
+  validate() {
+    validateform();
+  }
 
   constructor() { }
 
