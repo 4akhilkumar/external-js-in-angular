b9c3a407a33143a0800237e17b378918c10f861d
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-07 21:55:39 +0530

    Imported JsValidationFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsValidationFormComponent } from './js-validation-form/js-validation-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsValidationFormComponent
   ],
   imports: [
     BrowserModule,
