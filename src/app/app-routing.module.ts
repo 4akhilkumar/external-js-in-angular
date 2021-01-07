b4b7774b1fe50fcb904a12adcfe63b9162a70077
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-01-07 22:05:21 +0530

    routing - path is added for JsValidationFormComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JsValidationFormComponent } from './js-validation-form/js-validation-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'eptask',component:JsValidationFormComponent},
+  {path:'',component:JsValidationFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
